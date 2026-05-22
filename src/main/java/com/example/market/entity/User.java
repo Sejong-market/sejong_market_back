50ebe5708833aa2be3a20382e08142c594a3d54c
lakdja1234 <>
2026-05-22 15:38:03 +0900

    Feat: 회원 관리를 위한 User 엔티티 구현

--- src/main/java/com/example/market/entity/User.java
+++ src/main/java/com/example/market/entity/User.java
@@ -1,4 +1,42 @@
 package com.example.market.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "userid")
+	private Long userId;
+
+	@Column(nullable = false, unique = true, length = 45)
+	private String email;
+
+	@Column(nullable = false, length = 255)
+	private String password;
+
+	@Column(nullable = false, length = 45)
+	private String nickname;
+
+	@CreatedDate
+	@Column(name = "created_at", nullable = false, updatable = false)
+	private LocalDateTime createdAt;
 }
