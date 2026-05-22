c09437ab69ae4a6a189477ed2a4e719d781e8cfa
lakdja1234 <>
2026-05-22 16:23:38 +0900

    Feat: 회원가입 비즈니스 로직(이메일 중복 체크 및 저장) UserService 구현

--- src/main/java/com/example/market/service/UserService.java
+++ src/main/java/com/example/market/service/UserService.java
@@ -1,4 +1,33 @@
 package com.example.market.service;
 
+import com.example.market.dto.user.UserRequestDto;
+import com.example.market.entity.User;
+import com.example.market.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Transactional
+    public void signup(UserRequestDto requestDto) {
+
+        Optional<User> found = userRepository.findByEmail(requestDto.getEmail());
+        if (found.isPresent()) {
+            throw new IllegalArgumentException("이미 존재하는 이메일입니다.");
+        }
+
+        User user = new User(
+            requestDto.getEmail(),
+            requestDto.getPassword(),
+            requestDto.getNickname()
+        );
+
+        userRepository.save(user);
 }
