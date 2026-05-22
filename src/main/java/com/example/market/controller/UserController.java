9e98fd4971cb03b50beaf3f9a573973c5bf321d4
lakdja1234 <>
2026-05-22 16:30:51 +0900

    Feat: 회원가입 API 처리를 위한 UserController 구현

--- src/main/java/com/example/market/controller/UserController.java
+++ src/main/java/com/example/market/controller/UserController.java
@@ -1,4 +1,28 @@
 package com.example.market.controller;
 
+import com.example.market.dto.user.UserRequestDto;
+import com.example.market.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<Void> signup(@RequestBody UserRequestDto requestDto) {
+        try {
+            userService.signup(requestDto);
+            
+            return ResponseEntity.ok("회원가입 성공");
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
 }
