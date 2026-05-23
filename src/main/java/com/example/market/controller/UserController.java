598c946305745ccf0e2f30e0a01eb7ae6add10b3
lakdja1234 <>
2026-05-23 14:18:49 +0900

    Feat: UserController 로그인 성공 시 JWT 토큰 발급 기능 추가

--- src/main/java/com/example/market/controller/UserController.java
+++ src/main/java/com/example/market/controller/UserController.java
@@ -1,7 +1,10 @@
 package com.example.market.controller;
 
 import com.example.market.dto.user.UserRequestDto;
+import com.example.market.entity.User;
+import com.example.market.global.security.JwtUtil;
 import com.example.market.service.UserService;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+    private final JwtUtil jwtUtil;
 
     @PostMapping("/signUp")
     public ResponseEntity<Void> signUp(@RequestBody UserRequestDto requestDto) {
@@ -28,9 +32,16 @@ public class UserController {
 
 
     @PostMapping("/login")
-    public ResponseEntity<Void> login(@RequestBody UserRequestDto requestDto) {
+    public ResponseEntity<Void> login(
+            @RequestBody UserRequestDto requestDto,
+            HttpServletResponse response) {
         try {
-            userService.login(requestDto);
+            User user = userService.login(requestDto);
+
+            String token = jwtUtil.createToken(user.getEmail());
+
+            response.addHeader(jwtUtil.AUTHORIZATION_HEADER, token);
+
             return ResponseEntity.ok().build();
         } catch (IllegalArgumentException e) {
             return ResponseEntity.badRequest().build();
