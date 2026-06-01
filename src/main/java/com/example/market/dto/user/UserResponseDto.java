fdf432e6b01d31c9f4c593e927c6a2546c119424
lakdja1234 <159246@naver.com>
2026-06-01 16:42:28 +0900

    Feat: 내 정보 조회를 위한 UserResponseDto 추가

--- src/main/java/com/example/market/dto/user/UserResponseDto.java
+++ src/main/java/com/example/market/dto/user/UserResponseDto.java
@@ -1,4 +1,15 @@
 package com.example.market.dto.user;
 
+import com.example.market.entity.User;
+import lombok.Getter;
+
+@Getter
 public class UserResponseDto {
+    private final String email;
+    private final String nickname;
+
+    public UserResponseDto(User user) {
+        this.email = user.getEmail();
+        this.nickname = user.getNickname();
+    }
 }
